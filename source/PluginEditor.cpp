df541ddb0a2d7dd2534db42c02ec717fc6d7f166
Justin Garcia <functor@duck.com>
2023-10-18 12:33:31 +0800

    Initial commit

---
+++ source/PluginEditor.cpp
@@ -0,0 +1,53 @@
+#include "curl/curl.h"
+#include "PluginEditor.h"
+
+PluginEditor::PluginEditor (PluginProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+    juce::ignoreUnused (processorRef);
+
+    addAndMakeVisible (inspectButton);
+
+    // this chunk of code instantiates and opens the melatonin inspector
+    inspectButton.onClick = [&] {
+        if (!inspector)
+        {
+            inspector = std::make_unique<melatonin::Inspector> (*this);
+            inspector->onClose = [this]() { inspector.reset(); };
+        }
+
+        inspector->setVisible (true);
+    };
+
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+PluginEditor::~PluginEditor()
+{
+}
+
+void PluginEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    auto area = getLocalBounds();
+    g.setColour (juce::Colours::white);
+    g.setFont (16.0f);
+
+    curl_version_info_data* data = curl_version_info(CURLVERSION_NOW);
+    auto curlVersion = juce::String(data->version);
+
+    auto helloWorld = juce::String ("Hello from ") + PRODUCT_NAME_WITHOUT_VERSION + " v" VERSION + " running in " + CMAKE_BUILD_TYPE + " with curl: " + curlVersion;
+    g.drawText (helloWorld, area.removeFromTop (150), juce::Justification::centred, false);
+}
+
+void PluginEditor::resized()
+{
+    // layout the positions of your child components here
+    auto area = getLocalBounds();
+    area.removeFromBottom(50);
+    inspectButton.setBounds (getLocalBounds().withSizeKeepingCentre(100, 50));
+}
